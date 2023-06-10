4f51c3cf62a2930145da3858a449abfca990ac19
Eduardo <93436940+Eduardosmv@users.noreply.github.com>
2023-06-10 10:06:54 -0300

    Update index.js

--- prime/src/pages/Home/index.js
+++ prime/src/pages/Home/index.js
@@ -1,7 +1,47 @@
+import {useEffect, useState} from 'react';
+import api from '../../sevices/Api';
+
+
+//https://api.themoviedb.org/3//movie/now_playing?api_key=96076cdec05805e988903bffa64c5e6c&language=pt-BR
+
 function Home(){
+
+    const [filmes, setfilmes ]= useState ([]);
+
+
+useEffect(()=>{
+    async function loadFilmes(){
+        const response = await api.get("movie/now_playing",{
+                params:{
+                    api_key:"96076cdec05805e988903bffa64c5e6c",
+                    language: "pt-BR",
+                    PAGE: 1,
+                }
+
+        })
+       // console.log(response.data.results);
+       setfilmes(response.data.results.slice(0, 17))
+
+    }
+    loadFilmes();
+
+
+},[])
+
     return(
-        <div>
-            <h1>BEM VINDO A PAGINA HOME</h1>
+        <div className="container">
+            <div className="lista-filmes">
+                {filmes.map((filme)=>{
+                    return(
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}/>
+                        </article>
+                    )
+                })}
+
+            </div>
+            
         </div>
     )
 }
