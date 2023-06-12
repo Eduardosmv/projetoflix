1da06da2f008a5611acc2b57f12c6f097545cb18
Eduardo <93436940+Eduardosmv@users.noreply.github.com>
2023-06-12 11:50:50 -0300

    Update routes.js

--- prime/src/routes.js
+++ prime/src/routes.js
@@ -1,7 +1,8 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Erro from './pages/Erro';
 
 import Home from './pages/Home';
-import Filme from './pages/Filme'
+import Filme from './pages/Filme';
 import Header from './components/Header';
 
 function RoutesApp(){
@@ -10,7 +11,8 @@ function RoutesApp(){
           <Header/>
         <Routes>
            <Route path="/" element={ <Home/> }/>
-           <Route path="/Fime/:id" element={<Filme/>}/>
+           <Route path="/Filme/:id" element={<Filme/>}/>
+           <Route path='*' element={<Erro/>}/>
 
         </Routes>
         </BrowserRouter>
