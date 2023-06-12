3c79d78d3b03ab0e9faba34d759a4f6410b98c70
Eduardo <93436940+Eduardosmv@users.noreply.github.com>
2023-06-12 08:11:02 -0300

    Update index.js

--- prime/src/pages/Filme/index.js
+++ prime/src/pages/Filme/index.js
@@ -1,7 +1,12 @@
+import{ useEffect, useState} from 'react';
+import {useParams } from 'react-router-dom';
+
+
 function Filme(){
+    const {id} = useParams();
     return(
         <div>
-           <h1>Bem vindo aos filmes</h1>
+           <h1>Bem vindo aos filmes {id}</h1>
         
         </div>
     )
