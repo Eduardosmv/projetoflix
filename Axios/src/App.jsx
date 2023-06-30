8c8347c02216c235c4c1b2ba068bf828288f4d69
Eduardo <93436940+Eduardosmv@users.noreply.github.com>
2023-06-30 16:57:32 -0300

    Update App.jsx

--- Axios/src/App.jsx
+++ Axios/src/App.jsx
@@ -6,8 +6,10 @@ import axios from 'axios'
 function App(){
 
 const [dados, setdados] = useState([])
+//const cors = require("cors");
 
 const getdados = async() =>{
+ // App.use(cors());
 
   try{
     const response = await axios.get("http://10.0.1.43");
