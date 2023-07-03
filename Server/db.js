b2bfb4b55b35e80a5167a287bb527ef6011e906c
Eduardo <93436940+Eduardosmv@users.noreply.github.com>
2023-07-03 16:56:00 -0300

    DADOS POSTERGRES

--- Server/db.js
+++ Server/db.js
@@ -1,9 +1,48 @@
-async function connect(){
 
-    const {pool} = require("pg");
+    async function connect(){
+        if(global.connection)
 
-    const pool = new PoolCluster({
+        return global.connection.connect();
+
+    const { Pool } = require("pg");
+
+    const pool = new Pool({
         connectionString: process.env.CONNECTION_STRING
     });
 
+    const client = await pool.connect();
+    console.log("Criou o pool de conexão");
+
+    const res = await client.query("select now()");
+    console.log(res.rows[0]);
+    client.release();
+
+    global.connection = pool;
+
+    return pool.connect();  
+
+
+}
+connect();
+
+//LISTANDO DADOS DO BANCO DE DADOS
+
+async function selectCustomers(){
+
+   const modulo = await connect();  //conectando ao banco de dados
+   const res = await modulo.query("SELECT * FROM Modulos"); //enviando comando para o banco de dados via client.query e dentro de ("o comando SQL para buscar os dados")
+   return res.rows;
+}
+
+async function insertCustomer(customer){
+
+    const modulo = await connect();  //conectando ao banco de dados
+    const sql = "INSERT INTO modulos(MODULO, PULSOS, LOCALIZACAO, INICIAL, FINAL, ATUALIZACAO) VALUES ($1, $2, $3, $4, $5, $6)" ;
+    const values = [customer.MODULO, customer.PULSOS, customer.LOCALIZACAO, customer.INICIAL, customer.FINAL, customer.ATUALIZACAO ]
+    await modulo.query(sql, values);
+  
+}
+module.exports = {
+   // selectCustomers,
+    insertCustomer
 }
--- Server/index.js
+++ Server/index.js
@@ -1,14 +1,27 @@
 require("dotenv").config();
 
+const db = require("./db");
+
 const port = process.env.PORT;
 const express = require("express");
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.json({
         message: "FUNCIONANDO"
     })
 })
+app.get("/modulos", async (req, res) => {
+    const modulos = await db.selectCustomers();
+    res.json(modulos);
+})
+    app.post("/modulos", async (req, res) => {
+       const modulos = await db.insertCustomer(req.body);
+       res.sendStatus(201);
+    
+ })
 
 app.listen(port);
 
